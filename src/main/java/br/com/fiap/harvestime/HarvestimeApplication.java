7eb9bd8d95ed3cf390e1fc16cb148e1ff7fd46e9
Guilherme Cardoso <gcs200206@gmail.com>
2025-06-02 17:26:39 -0300

    first commit

---
+++ src/main/java/br/com/fiap/harvestime/HarvestimeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.harvestime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvestimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/harvestime/HarvestimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.harvestime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
